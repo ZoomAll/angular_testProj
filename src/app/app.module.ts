b7452a69940f2aa93f03a746526f3286d453ae49
z <alex@dwfe.ru>
2017-11-15 02:26:55 +0300

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {MyComponent} from './my/my.component';
+import { CarsComponent } from './cars/cars.component';
+import { CarComponent } from './car/car.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, MyComponent, CarsComponent, CarComponent
   ],
   imports: [
     BrowserModule
@@ -13,4 +16,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/my/my.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-my-comp',
+  template: `<h1>wrw</h1>`,
+})
+export class MyComponent {
+}
