50e7a3683c0c64d308af49c00a11ea4b84fe725a
z <alex@dwfe.ru>
2017-11-17 19:16:31 +0300

    form - R - Валидация формы и Группировка элементов

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 
 @Component({
@@ -17,14 +17,26 @@ export class AppComponent {
   }];
 
   form: FormGroup = new FormGroup({
-    email: new FormControl(''),
-    pass: new FormControl(''),
+    user: new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      pass: new FormControl('', Validators.required),
+    }),
     country: new FormControl('by'),
     answer: new FormControl('yes'),
   });
 
   onSubmit() {
     console.log('submited!', this.form);
+  }
+
+  isEmailInvalid(): boolean {
+    const email = this.form.get('user.email');
+    return email.invalid && email.touched;
+  }
+
+  isPassInvalid(): boolean {
+    const pass = this.form.get('user.pass');
+    return pass.invalid && pass.touched;
   }
 
 }
