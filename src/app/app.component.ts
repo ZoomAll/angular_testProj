29ff4e33ba66ea6946157d37d3e0178e9d717175
z <alex@dwfe.ru>
2017-11-17 20:43:11 +0300

    form - R - Создание асинхронного валидатора

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {reject} from "q";
-
 
 @Component({
   selector: 'app-root',
