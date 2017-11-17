9e49d16f708f4b7de740c60e70f2a9ce6be9f33d
z <alex@dwfe.ru>
2017-11-17 15:59:45 +0300

    form - TDr - Динамическое изменение формы

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import {NgForm} from '@angular/forms';
 
 
@@ -18,7 +18,31 @@ export class AppComponent {
   defaultAnswer = 'no';
   defaultCountry = 'ru';
 
-  submitForm(form: NgForm) {
-    console.log(form);
+  formData = {};
+  isSubmited = false;
+
+  @ViewChild('myLoginForm') form: NgForm;
+
+  submitForm() {
+    this.isSubmited = true;
+    this.formData = this.form.value;
+    this.form.reset();
+    console.log(this.form);
+  }
+
+  addRandEmail() {
+    // this.form.setValue({
+    //   user: {
+    //     pass: '',
+    //     email: 'rand@email.com',
+    //   },
+    //   country: '',
+    //   answer: '',
+    // });
+
+    this.form.form.patchValue({
+      user: {email: 'rand@email.com'}
+    });
+
   }
 }
